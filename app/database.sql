3367175a3d2a35c257683af604cfedb41fad0bff
ticket267speedy <luis.huapaya@utec.edu.pe>
2025-10-02 19:25:09 -0500

    Update database.sql

--- app/database.sql
+++ app/database.sql
@@ -3,6 +3,24 @@ CREATE DATABASE IF NOT EXISTS flask_crud_db CHARACTER SET utf8mb4 COLLATE utf8mb
 
 USE flask_crud_db;
 
+-- laboratorio 3
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    rol ENUM('admin', 'usuario') NOT NULL
+);
+
+INSERT INTO usuarios (nombre, email, password, rol) VALUES 
+('Administrador', 'admin@mail.com', 'admin123', 'admin'),
+('Carlos Vega', 'carlos@abc.com', 'userpass', 'usuario'),
+('Ana Luna', 'ana@abc.com', 'userpass', 'usuario');
+
+
+-- ########################################################################
+
+-- ESTO ES DE LAS CLASES DE COGNITIVE(OMITIR)
 CREATE TABLE IF NOT EXISTS productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
@@ -33,3 +51,4 @@ INSERT INTO cursos (nombre, creditos) VALUES
 ('Interconectados', 4),
 ('ICC', 5),
 ('Redes Digitales', 4);
+
